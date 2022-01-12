4714cd83edaedba0efab3aa938b6c094d3f1c2e4
GeovaniSS <geosilvasousa@gmail.com>
2022-01-11 22:24:27 -0300

    Problema com a imagem solucionado e formulário adicionado

---
+++ readme.mk
@@ -0,0 +1,12 @@
+## Alinhar o label no flex start do input
+## Alinhar a ícone do lado do input
+## Como aumentar o tamanho do input
+## Como estilizar o input type ="submit"
+## Pesquisar se os icones vão dentro ou fora do input 
+
+### Formas de utilizar ícones dentro do input 
+	- Position relative/absolute
+	- Label personalizado com ícone 
+
+## Animação com input focus
+	- Animação do border ou outline??
