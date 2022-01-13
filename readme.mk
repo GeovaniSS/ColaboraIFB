f4ba2db3596c53ca316a3e5dcb63a4a9f7254423
GeovaniSS <geosilvasousa@gmail.com>
2022-01-12 22:38:04 -0300

    Alteração na Página de Login / Versão 1.0

--- readme.mk
+++ readme.mk
@@ -5,8 +5,13 @@
 ## Pesquisar se os icones vão dentro ou fora do input 
 
 ### Formas de utilizar ícones dentro do input 
-	- Position relative/absolute
-	- Label personalizado com ícone 
+	- Position relative/absolute 
+	- Label personalizado com ícone (font-awesome)
 
 ## Animação com input focus
-	- Animação do border ou outline??
+	- Animação do border
+	- Como fazer com que o ícone mude de característica junto com o input:focus
+		- utilize a concatenação (input:focus + label)
+	- 
+
+## Adicionar box-shadow no input
