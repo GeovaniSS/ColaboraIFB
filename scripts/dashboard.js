88a00a01b57a0daf637457f043a079ac1d533d33
GeovaniSS <geosilvasousa@gmail.com>
2022-01-29 00:03:02 -0300

    Script dos cards externos na dashboard

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,5 +1,31 @@
 /*======================================== SISTEMAS EXTERNOS ======================================== */
-var Nead = document.getElementsByClassName('external-card')[0]
+const externalCardLink = document.querySelectorAll('div.external-card')
+const cardsLength = externalCardLink.length
+
+for (let i = 0; i < cardsLength; i++) {
+    externalCardLink[i].addEventListener('click', acessarExterno)
+    function acessarExterno() {
+        switch (i) {
+            case 0:
+                window.open('https://nead.ifb.edu.br/', "_blank")
+                break
+        
+            case 1:
+                window.open('https://portaldoestudante.ifb.edu.br/', "_blank")
+                break
+                
+            case 2:
+                window.open('https://plataforma.bvirtual.com.br/', "_blank")
+                break
+        
+            case 3:
+                window.open('https://www.gmail.com/', "_blank") 
+                break
+        }
+    }
+}
+
+/*var Nead = document.getElementsByClassName('external-card')[0]
 var Portal = document.getElementsByClassName('external-card')[1]
 var Biblioteca = document.getElementsByClassName('external-card')[2]
 var Email = document.getElementsByClassName('external-card')[3]
@@ -23,7 +49,7 @@ function acessarEmail () {
 Nead.addEventListener('click', acessarNead)
 Portal.addEventListener('click', acessarPortal)
 Biblioteca.addEventListener('click', acessarBiblioteca)
-Email.addEventListener('click', acessarEmail)
+Email.addEventListener('click', acessarEmail)*/
 
 
 /*======================================== AULAS SÍNCRONAS ======================================== */
